f58e1c143dca25ac23cca64985e1fcada7b5815d
Susmita Poudel <110353867+susmita-p2@users.noreply.github.com>
2023-03-23 22:26:58 -0400

    Update MergeIntervals.py

--- Assignment-1/MergeIntervals.py
+++ Assignment-1/MergeIntervals.py
@@ -1,9 +1,14 @@
 #Question 8: Given a list of integer pairs representing the low and high end of an interval, inclusive, return a list in which overlapping intervals are merged.
 #Hash
 #Time Complexity : O(nlogn).    Space Complexity: O(n)
+
 def merge_intervals(arr):
 
-    arr = sorted(arr, key = lambda x:x[0])
+    arr = merge_sort(arr)
+    
+    if len(arr) <= 1:
+        return arr
+
     merged = []
     merged.append((arr[0]))
    
@@ -16,6 +21,37 @@ def merge_intervals(arr):
             merged.append((interval))
 
     return merged
+
+#Merge_sort code   
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left_half, right_half):
+    result = []
+    i = j = 0
+
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i][0] < right_half[j][0]:
+            result.append(left_half[i])
+            i += 1
+        else:
+            result.append(right_half[j])
+            j += 1
+
+    result += left_half[i:]
+    result += right_half[j:]
+
+    return result
 
    
 
