7284fda46f8fc6a40eef1c10fe042d614726b0fa
Susmita Poudel <susmita.poudel@centre.edu>
2023-06-21 09:57:45 -0400

    Created Q4.py

---
+++ Assigment-3/Q4.py
@@ -0,0 +1,2 @@
+#Question 4: NumberOfIslands
+#Given a binary matrix in which 1s represent land and 0s represent water. Return the number of islands (contiguous 1s surrounded by 0s or the edge of the matrix).
