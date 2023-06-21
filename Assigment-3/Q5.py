41cacf1cd2339bb4891f9025e5a8f05a75de53e2
Susmita Poudel <susmita.poudel@centre.edu>
2023-06-21 15:55:05 -0400

    Updated Q5.py

--- Assigment-3/Q5.py
+++ Assigment-3/Q5.py
@@ -1,6 +1,7 @@
 #Question 5: FirstKBinaryNumbers
 #Given a number, k, return an array of the first k binary numbers, represented as strings.
-
+#Queue
+#Time-complexity: O(n), Space-complexity: O(n)
 def firstKBinaryNumbers(num):
     result = []
     queue = ['0','1']
@@ -10,21 +11,23 @@ def firstKBinaryNumbers(num):
         return result
     if num == 1:
          return queue
-    while len(result) < num:
+    while len(result) <= num:
         number = queue.pop(0)
         result.append(number)
         if number != '0':
             queue.append(number + '0')
             queue.append(number + '1')
 
-    return result
+    return result[:num]
 
 def main():
      print(firstKBinaryNumbers(5))
      print(firstKBinaryNumbers(10))
      print(firstKBinaryNumbers(0))
-     print(firstKBinaryNumbers(1))
+     print(firstKBinaryNumbers(2))
 
 
 if __name__ == '__main__':
         main()
+
+#Time-spent:32 minutes
