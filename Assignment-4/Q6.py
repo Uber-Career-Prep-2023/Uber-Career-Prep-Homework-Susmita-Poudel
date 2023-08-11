27c4c8e186544bbe21f8cee203007c0d2f77ba1f
Susmita Poudel <susmita.poudel@centre.edu>
2023-08-10 22:07:03 -0400

    Updated Q6.py

--- Assignment-4/Q6.py
+++ Assignment-4/Q6.py
@@ -2,15 +2,24 @@
 #Given a string of characters without spaces and a dictionary of valid words, determine if 
 #it can be broken into a list of valid words by adding spaces. 
 
+#Time-complexity: 0(n^2)   Space-complexity: 0(n) n = length of input string 's'
+
+def wordBreak(s, wordDict):
+    n = len(s)
+    dp = [False] * (n + 1)
+    dp[0] = True  # Empty string is a valid word
+    s = s.lower()
+    for i in range(1, n + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in wordDict:
+                dp[i] = True
+                break
+    
+    return dp[n]
 
 
-#I couldnt figure out how to solve this problem :)
-
-
-
-# Example usage:
 def main():
-    wordDict = {"Elf", "Go", "Golf", "Man", "Manatee", "Not", "Note", "Pig", "Quip", "Tee", "Teen"}
+    wordDict = {"elf", "go", "golf", "man", "manatee", "not", "note", "pig", "quip", "tee", "teen"}
 
     input1 = "mangolf"
     print(wordBreak(input1, wordDict))  # Output: True
